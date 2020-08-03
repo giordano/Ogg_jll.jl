dfc66ea52bd5a61f03b43814b8ee0a1f7a7965e7
jlbuild <juliabuildbot@gmail.com>
2020-08-03 20:02:32 +0000

    Ogg_jll build 1.3.4+1

--- src/wrappers/x86_64-apple-darwin14.jl
+++ src/wrappers/x86_64-apple-darwin14.jl
@@ -5,6 +5,7 @@ export libogg
 PATH = ""
 LIBPATH = ""
 LIBPATH_env = "DYLD_FALLBACK_LIBRARY_PATH"
+LIBPATH_default = "~/lib:/usr/local/lib:/lib:/usr/lib"
 
 # Relative path to `libogg`
 const libogg_splitpath = ["lib", "libogg.0.dylib"]
@@ -28,8 +29,6 @@ function __init__()
 
     # Initialize PATH and LIBPATH environment variable listings
     global PATH_list, LIBPATH_list
-    # We first need to add to LIBPATH_list the libraries provided by Julia
-    append!(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
     global libogg_path = normpath(joinpath(artifact_dir, libogg_splitpath...))
 
     # Manually `dlopen()` this right now so that future invocations
@@ -41,12 +40,8 @@ function __init__()
     filter!(!isempty, unique!(PATH_list))
     filter!(!isempty, unique!(LIBPATH_list))
     global PATH = join(PATH_list, ':')
-    global LIBPATH = join(LIBPATH_list, ':')
+    global LIBPATH = join(vcat(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ':')
 
-    # Add each element of LIBPATH to our DL_LOAD_PATH (necessary on platforms
-    # that don't honor our "already opened" trick)
-    #for lp in LIBPATH_list
-    #    push!(DL_LOAD_PATH, lp)
-    #end
+    
 end  # __init__()
 
